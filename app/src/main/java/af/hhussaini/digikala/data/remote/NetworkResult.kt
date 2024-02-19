1329491235440207102acfc09b1d518bd9512dad
HHussainiaf <155997481+HHussainiaf@users.noreply.github.com>
2024-02-19 15:03:06 +0330

    Initial commit

---
+++ app/src/main/java/af/hhussaini/digikala/data/remote/NetworkResult.kt
@@ -0,0 +1,12 @@
+package af.hhussaini.digikala.data.remote
+
+sealed class NetworkResult<T>(
+    val message: String? = null,
+    val data: T? = null
+) {
+
+    class Success<T>(message: String , data: T): NetworkResult<T>(message, data)
+    class Error<T>(message: String, data: T): NetworkResult<T>(message, data)
+    class Loading<T>: NetworkResult<T>()
+
+}
